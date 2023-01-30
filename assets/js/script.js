19c0926b4bab19e2b0f8462dfe5115c85739ffc8
JillKerrB <jillianannkerr@gmail.com>
2023-01-30 17:49:26 +0000

    update some typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons=document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
@@ -8,15 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType=this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
 
     }
     runGame("addition");
-})
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 
 function runGame(gameType){
-    let num1=(math.floor(math.random) * 25) +1;
-    let num2=(math.floor(math.random) * 25) +1;
+
+    // Creates two random numbers between 1 and 25
+    let num1=Math.floor(Math.random() * 25) +1;
+    let num2=Math.floor(Math.random() * 25) +1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
